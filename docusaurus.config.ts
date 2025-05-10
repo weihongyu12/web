c252cdc26d71a2381edfd901b7e9863c4ad486aa
weihongyu12 <weihongyu12@outlook.com>
2025-05-10 21:26:05 +0800

    调整侧边栏配置，添加流水线和功能设计文档链接

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,24 +90,24 @@ const config: Config = {
           position: 'left',
           label: '参考',
         },
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'deploySidebar',
+        //   position: 'left',
+        //   label: '部署',
+        // },
         {
           type: 'docSidebar',
-          sidebarId: 'deploySidebar',
+          sidebarId: 'pipelineSidebar',
           position: 'left',
-          label: '部署',
+          label: '流水线',
         },
-        // {
-        //   type: 'docSidebar',
-        //   sidebarId: 'pipelineSidebar',
-        //   position: 'left',
-        //   label: '流水线',
-        // },
-        // {
-        //   type: 'docSidebar',
-        //   sidebarId: 'featuresSidebar',
-        //   position: 'left',
-        //   label: '功能设计',
-        // },
+        {
+          type: 'docSidebar',
+          sidebarId: 'featuresSidebar',
+          position: 'left',
+          label: '功能设计',
+        },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
