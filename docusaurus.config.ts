d33881d4fb6aa1c66fe1cea11a14451ce2691423
weihongyu12 <weihongyu12@outlook.com>
2025-07-18 15:46:31 +0800

    更新文档路径重定向配置，优化用户访问体验

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -174,12 +174,12 @@ const config: Config = {
       {
         redirects: [
           {
-            to: '/web/docs/features/authentication/smscode/',
-            from: ['/web/docs/features/', '/web/docs/features/authentication/'],
+            to: '/docs/features/authentication/smscode/',
+            from: ['/docs/features/', '/docs/features/authentication/'],
           },
           {
-            to: '/web/docs/features/payment/alipay/',
-            from: ['/web/docs/features/payment/'],
+            to: '/docs/features/payment/alipay/',
+            from: ['/docs/features/payment/'],
           },
         ],
       },
