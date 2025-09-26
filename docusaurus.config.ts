8f5761e959678506b98fd4b5fe2151ea8f6df29b
weihongyu12 <weihongyu12@outlook.com>
2025-09-26 21:59:16 +0800

    更新docusaurus@3.9.1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,6 @@ const config: Config = {
   projectName: 'weihongyu12.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -173,6 +172,9 @@ const config: Config = {
 
   themes: ['@docusaurus/theme-mermaid'],
   markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
     mermaid: true,
   },
 
