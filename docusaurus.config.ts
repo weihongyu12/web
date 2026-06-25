148e212aa82353418ded6b3a685ea8267ba2a103
weihongyu12 <weihongyu12@outlook.com>
2026-06-25 16:55:30 +0800

    新增小程序版的导航链接

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -108,6 +108,11 @@ const config: Config = {
         },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
+          href: 'https://weihongyu12.github.io/miniprogram/',
+          label: '小程序版',
+          position: 'right',
+        },
+        {
           href: 'https://github.com/weihongyu12',
           label: 'GitHub',
           position: 'right',
